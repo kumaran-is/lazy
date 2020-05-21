d60ebccfccc256aa36c5f9534d13de7134d0f013
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2020-05-20 20:21:27 -0400

    first commit

---
+++ src/app/feature1/feature1.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Feature1Component } from './feature1.component';
+
+
+
+@NgModule({
+  declarations: [Feature1Component],
+  imports: [
+    CommonModule
+  ]
+})
+export class Feature1Module { }
